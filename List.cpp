ce29cb1655745edf9397010bdd7652d091dea8d5
Mishka-3 <91006679+Mishka-3@users.noreply.github.com>
2021-12-11 12:09:22 +0300

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -6,6 +6,7 @@
 #include "Fiveangle.h"
 #include "Item.h"
 
+
 using namespace std;
 
 List::List()
@@ -13,13 +14,11 @@ List::List()
       head= NULL;
       tail= NULL;
       size=0;
-      std::cout << "List1 created" << std::endl;
 }
 List::List(const List& orig) {
       size= orig.size;
       head= orig.head;
       tail= orig.tail;
-      std::cout << "List2 created" << std::endl;
 }
 List::~List()
 {
@@ -35,60 +34,33 @@ void List::DelAll()
       Del(1);
 }
 
+void List::Del(size_t index) {
+    Item* elm = NULL;
+    Fiveangle tmp;
+    elm = GetItem(index);
+    if (elm == NULL) {
+        exit(5);
+    }
+    if (elm->prev) {
+        elm->prev->next = elm->next;
+    }
+    if (elm->next) {
+        elm->next->prev = elm->prev;
+    }
+    tmp = elm->fiveangle;
 
-void List::Del(int pos)
-{
-   // если параметр отсутствует или равен 0, то запрашиваем его
-   if(pos == 0)
-   {
-       cout << "Input position: ";
-       cin >> pos;
-   }
-   // Позиция от 1 до size?
-   if(pos < 1 || pos > size)
-   {
-      // Неверная позиция
-      cout << "Incorrect position !!!\n";
-      return;
-   }
+    if (!elm->prev) {
+        head = elm->next;
+    }
+    if (!elm->next) {
+        tail = elm->prev;
+    }
 
-   // Счетчик
-   int i = 1;
+    free(elm);
 
-   Item * Del = head;
-
-   while(i < pos)
-   {
-      // Доходим до элемента,
-      // который удаляется
-      Del = Del->next;
-      i++;
-   }
-
-   // Доходим до элемента,
-   // который предшествует удаляемому
-   Item * PrevDel = Del->prev;
-   // Доходим до элемента, который следует за удаляемым
-   Item * AfterDel = Del->next;
-
-   // Если удаляем не голову
-   if(PrevDel != 0 && size != 1)
-      PrevDel->next = AfterDel;
-   // Если удаляем не хвост
-    if(AfterDel != 0 && size != 1)
-      AfterDel->prev = PrevDel;
-
-   // Удаляются крайние?
-   if(pos == 1)
-       head = AfterDel;
-   if(pos == size)
-       tail = PrevDel;
-
-   // Удаление элемента
-   delete Del;
-
-   size--;
+    size--;
 }
+
 
 void List::AddHead(Fiveangle& fiveangle)
 {
@@ -96,14 +68,14 @@ void List::AddHead(Fiveangle& fiveangle)
    Item * temp = new Item(fiveangle);
 
    // Предыдущего нет
-   temp->prev = 0;
+   temp->prev = NULL;
    // Заполняем данные
    temp->fiveangle = fiveangle;
    // Следующий - бывшая голова
    temp->next = head;
 
    // Если элементы есть?
-   if(head != 0)
+   if(head != NULL)
       head->prev = temp;
 
    // Если элемент первый, то он одновременно и голова и хвост
@@ -140,7 +112,6 @@ void List::AddTail(Fiveangle& fiveangle)
 
    size++;
 
-   cout << "size=" << size << "\n";
 }
 
 void List::Insert(int pos)
@@ -164,7 +135,7 @@ void List::Insert(int pos)
    if(pos == size + 1)
    {
       Fiveangle tmp;
-      cout << "Введите координаты вершин\n";
+      cout << "coordinates of the vertices\n";
       cin >> tmp;
       // Добавление в конец списка
       AddTail(tmp);
@@ -174,7 +145,7 @@ void List::Insert(int pos)
    {
       // Вставляемые данные
        Fiveangle tmp;
-       cout << "Введите координаты вершин\n";
+       cout << "coordinates of the vertices\n";
        cin >> tmp;
       // Добавление в начало списка
       AddHead(tmp);
@@ -203,13 +174,13 @@ void List::Insert(int pos)
 
    // Вставляемые данные
     Fiveangle tmp;
-    cout << "Введите координаты вершин\n";
+    cout << "coordinates of the vertices\n";
     cin >> tmp;
     Item * temp = new Item(tmp);
 
 
    // настройка связей
-   if(PrevIns != 0 && size != 1)
+   if(PrevIns != NULL && size != 1)
       PrevIns->next = temp;
 
    temp->next = Ins;
@@ -227,7 +198,7 @@ void List::Print()
    if(size != 0)
    {
       Item * temp = head;
-      while(temp->next != 0)
+      while(temp->next != NULL)
       {
           cout << temp->fiveangle;
           temp = temp->next;
@@ -237,16 +208,7 @@ void List::Print()
    }
 }
 
-/*std::ostream& operator<<(std::ostream& os, const List& list) {
 
-    Item *item = list.head;
-    while(item!=NULL)
-    {
-        os << item->fiveangle;
-        item = item->GetNext();
-    }
-    return os;
-}*/
 std::ostream& operator<<(std::ostream& os, const List& list) {
     Item* item = list.head;
     while (item != NULL)
@@ -256,20 +218,6 @@ std::ostream& operator<<(std::ostream& os, const List& list) {
     }
     return os;
 }
-
-/*ostream& operator<<(ostream& os, const List& list) {
-    os.precision(3);
-    Item* item = list.head;
-    auto s = new double[list.size];
-    for (int i = 0; i < list.size; ++i) {
-        s[i] = item->GetFiveangle().get_Area();
-        item = item->GetNext();
-    }
-    for (int i = (int) list.size - 1; i >= 0; --i) {
-        os << "=> " << s[i] << " ";
-    }
-    return os;
-}*/
 
 Item* List::GetItem(int pos)
 {
@@ -285,11 +233,18 @@ Item* List::GetItem(int pos)
 
    int i = 1;
    // Ищем нужный нам элемент
-   while(i < pos && temp != 0)
+   while(i < pos && temp != NULL)
    {
       temp = temp->next;
       i++;
    }
+
+   if(temp == NULL)
+      return 0;
+   else
+      return temp;
+}
+
 
    if(temp == 0)
       return 0;
